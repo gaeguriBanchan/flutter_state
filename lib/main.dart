f24c4e395fd458b895be7cc9abc9f40c6140fb0e
gaeguriBanchan <rlawowls93@gamil.com>
2023-08-02 19:09:37 +0900

    Widget Lifecycle

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,14 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  bool showTitle = true;
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,13 +30,17 @@ class _AppState extends State<App> {
           ),
         ),
       ),
-      home: const Scaffold(
-        backgroundColor: Color(0xFFF4EDDB),
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              MyLargeTitle(),
+              showTitle ? const MyLargeTitle() : const Text('nothing'),
+              IconButton(
+                onPressed: toggleTitle,
+                icon: const Icon(Icons.remove_red_eye),
+              )
             ],
           ),
         ),
@@ -37,13 +49,38 @@ class _AppState extends State<App> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     super.key,
   });
 
   @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  int counter = 0;
+
+  // initState 는 build 이전에 실행됨.
+  // 부모 파라미터에 의존할때 그것을 초기화 할 때 사용
+  @override
+  void initState() {
+    super.initState();
+    print('init');
+  }
+
+  // disposes 는 위젯이 스크린에서 제거될때 호출
+  // API 업데이트, 이벤트리스너 구독취소, form 리스너 벗어날때 등등
+  // 무언가 취소할 때 사용
+  @override
+  void dispose() {
+    super.dispose();
+    print('dispose');
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print('build');
     return Text(
       'Click Counter',
       style: TextStyle(
